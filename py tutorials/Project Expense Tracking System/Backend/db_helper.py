1e16c15181fabd92a263ecbb7198f6d2de54a72e
vaibhav avhad <vaibhavavhad264@gmail.com>
2026-07-10 13:59:08 +0530

    Here is some code for the project

--- py tutorials/Project Expense Tracking System/Backend/db_helper.py
+++ py tutorials/Project Expense Tracking System/Backend/db_helper.py
@@ -1,6 +1,9 @@
 import mysql.connector
 from contextlib import contextmanager
-from datetime import date
+from logging_setup import setup_logger
+
+logger = setup_logger("db_helper")
+
 
 @contextmanager
 def get_db_cursor(commit = False):
@@ -22,31 +25,27 @@ def get_db_cursor(commit = False):
         cursor.close()
         connection.close()
 
-def fetch_all_records():
-    with get_db_cursor() as cursor:
-        cursor.execute("select * from expenses")
-        expenses = cursor.fetchall()
-        return expenses
-
-
 def fetch_by_date(expense_date):
+    logger.info(f"Fetching expense by date {expense_date}")
     with get_db_cursor() as cursor:
         cursor.execute("select * from expenses where expense_date = %s", (expense_date,))
         expenses = cursor.fetchall()
         return expenses
 
 def insert_expenses(expense_date, amount, category, notes):
+    logger.info(f"Insert expense by date: {expense_date}, amount : {amount}, category : {category}, notes : {notes}")
     with get_db_cursor(commit = True) as cursor:
         cursor.execute("insert into expenses (expense_date, amount, category, notes) values (%s, %s, %s, %s)",
                        (expense_date, amount, category, notes))
 
 def delete_expenses_for_date(expense_date):
+    logger.info(f"Delete expense by date: {expense_date}")
     with get_db_cursor() as cursor:
         cursor.execute("delete from expenses where expense_date = %s", (expense_date,))
 
-    print("Expenses deleted successfully")
 
 def fetch_expense_summary(st_date, end_date):
+    logger.info(f"Fetching expense summary by date {st_date} and end date {end_date}")
     with get_db_cursor() as cursor:
         cursor.execute('''SELECT category, SUM(amount) AS total 
                        FROM expenses WHERE expense_date 
@@ -56,15 +55,20 @@ def fetch_expense_summary(st_date, end_date):
         return data
 
 if __name__ == "__main__":
-    # exp = fetch_by_date("2024-08-20")
-    # for expenses in exp:
-    #     print(expenses)
+
+    exp = fetch_by_date("2024-08-01")
+    print(exp)
+    delete_expenses_for_date("2024-08-25")
+    summary = fetch_expense_summary("2024-08-01", "2024-08-05")
+    for data in summary:
+        print(data)
+
 
     # insert_expenses("2024-08-20", 1200, "Food", "ICE-CREAM")
     # exp = fetch_by_date("2024-08-20")
     # print(exp)
     #
-    delete_expenses_for_date("2024-08-20")
+
     # exp = fetch_by_date("2024-08-20")
     # print(exp)
 
---
+++ py tutorials/Project Expense Tracking System/Frontend/app.py
@@ -0,0 +1,48 @@
+import datetime
+
+import streamlit as st
+import pandas as pd
+
+# # test element
+# st.header("Streamlit Core Feature")
+# st.subheader("Test Element")
+# st.text("This is the simple test element")
+#
+# # Data display
+#
+# st.subheader("Data Display")
+# st.write("Here is simple table")
+#
+# df = pd.DataFrame({
+#     "Date" : ["2024-08-01", "2024-09-01", "2024-10-01", "2024-11-01", "2024-12-01"],
+#     "Amount" : [250, 490, 560, 780, 999]
+# })
+#
+# st.table(df)
+#
+# #charts
+#
+# st.subheader("Charts")
+# st.line_chart(df["Amount"])
+#
+# #User Input
+# st.subheader("User Input")
+# value = st.slider("Select a value", 0, 100)
+# st.write("Your Selected Value Is : ", value)
+#
+#
+
+st.title("Interactive Widget Example")
+
+if st.checkbox("SHOW/HIDE"):
+    st.write("Checkbox is checked")
+
+option = st.selectbox("Select a number", [1, 2, 3, 4])
+st.write(f" You Selected {option}")
+
+options = st.multiselect("Select an option", [1, 2, 3, 4])
+st.write(f" You Selected {options}")
+
+
+
+
